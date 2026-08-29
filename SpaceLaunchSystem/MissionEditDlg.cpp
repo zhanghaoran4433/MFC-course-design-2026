962664e45441c84f20dd76330b2ad9536665c1b7
Zhang Haoran <443323145@qq.com>
2026-08-29 22:40:06 +0800

    feat: add mission edit dialog skeleton

---
+++ SpaceLaunchSystem/MissionEditDlg.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "MissionEditDlg.h"
+
+IMPLEMENT_DYNAMIC(CMissionEditDlg, CDialogEx)
+
+CMissionEditDlg::CMissionEditDlg(CWnd* pParent)
+	: CDialogEx(IDD_MISSION_EDIT_DIALOG, pParent)
+{
+}
+
+void CMissionEditDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_MISSION_ID, m_mission.missionId);
+	DDX_Text(pDX, IDC_EDIT_MISSION_NAME, m_mission.missionName);
+	DDX_Text(pDX, IDC_EDIT_ROCKET_NAME, m_mission.rocketName);
+	DDX_Text(pDX, IDC_EDIT_PAYLOAD_NAME, m_mission.payloadName);
+	DDX_CBString(pDX, IDC_COMBO_DESTINATION, m_mission.destination);
+	DDX_Text(pDX, IDC_EDIT_LAUNCH_TIME, m_mission.launchTime);
+}
+
+BEGIN_MESSAGE_MAP(CMissionEditDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+BOOL CMissionEditDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	CComboBox* pDestinationCombo =
+		static_cast<CComboBox*>(GetDlgItem(IDC_COMBO_DESTINATION));
+	if (pDestinationCombo != nullptr)
+	{
+		pDestinationCombo->AddString(L"近地轨道");
+		pDestinationCombo->AddString(L"月球");
+		pDestinationCombo->AddString(L"火星");
+		pDestinationCombo->AddString(L"空间站");
+	}
+
+	UpdateData(FALSE);
+	return TRUE;
+}
---
+++ SpaceLaunchSystem/MissionEditDlg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "DataTypes.h"
+#include "Resource.h"
+
+class CMissionEditDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CMissionEditDlg)
+
+public:
+	explicit CMissionEditDlg(CWnd* pParent = nullptr);
+
+	enum { IDD = IDD_MISSION_EDIT_DIALOG };
+
+	MissionInfo m_mission;
+	BOOL m_isModifyMode = FALSE;
+
+	virtual BOOL OnInitDialog();
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+	DECLARE_MESSAGE_MAP()
+};
